ca20747e3e2eb0267655145e8d0d69dfa6f273f9
Sarah Moffat <sarah.moffat5@gmail.com>
2022-02-03 15:32:22 +0000

    Answered step 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,10 +4,11 @@ class Oystercard
   MINIMUM_FARE = 1
   MINIMUM_BALANCE = 1
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station, :journeys
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @journeys = {}
   end
 
   def top_up(amount)
@@ -19,19 +20,18 @@ class Oystercard
     !!entry_station
   end
 
-  def touch_in(entry_station)
+  def touch_in(station)
     fail "Insufficient funds for travel - balance below #{MINIMUM_BALANCE}" if balance < MINIMUM_BALANCE
-    @entry_station = entry_station
+    @entry_station = station
+    @journeys[:entry_station] = station
   end
 
-  def touch_out
+  def touch_out(exit_station)
     deduct(MINIMUM_FARE)
+    @exit_station = exit_station
+    @journeys[:exit_station] = exit_station
     @entry_station = nil
   end
-
-  #def insufficient_funds?
-  #  @balance < MINIMUM_BALANCE
-  #end
 
   private
 
